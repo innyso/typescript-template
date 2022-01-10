2cd504262fd70f8f94d21473d8c309dacb7145cd
Inny So <iso@thoughtworks.com>
2022-01-10 16:11:02 +1100

    fix typo

--- src/calc/calc.test.ts
+++ src/calc/calc.test.ts
@@ -1,6 +1,6 @@
 import { add } from "./calc";
 
-describe("tets add function", () => {
+describe("test add function", () => {
   it("should return 15 for add(10, 5)", () => {
     expect(add(10, 5)).toBe(15);
   });
